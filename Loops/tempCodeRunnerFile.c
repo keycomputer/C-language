0b249651c66708cc13497179f3985656883a99e9
Key Computer <keycomputereducation@gmail.com>
2026-07-11 13:26:54 +0530

    pattern

--- Loops/tempCodeRunnerFile.c
+++ Loops/tempCodeRunnerFile.c
@@ -1,6 +1,34 @@
-or(i = 1; i <= 4 ;i++ )
+#include<stdio.h>
+int main()
+{
+    int r , i, j;
+    printf("enter row ");
+    scanf("%d", &r);
+
+    for(i=1;i<=r;i++)
     {
-        for(j = 1; j <= 4 ; j++)
-            printf("* ");
+        for(j=1;j<=i;j++)
+        {
+            printf("%d", i);
+        }
         printf("\n");
     }
+    for(i=1;i<=r;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    for(i=r;i>=1;i--)
+    {
+        for(j=r;j>=i;j--)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
